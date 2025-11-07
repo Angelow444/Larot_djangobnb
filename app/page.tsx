6c9c2812cfa836dd9b613618f9d63928c8bc7125
Angelow444 <angelow.larot@msunaawan.edu.ph>
2025-11-08 01:20:31 +0800

    1st Video_Categories on the front page -Angelow B. Larot

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import Categories from "./components/Categories";
+
 
 export default function Home() {
   return (
-    <main className="">
-      django bnb
-      <h2 className="Text-aibnb">Django</h2>
+    <main className="max-w-[1500px] mx-auto px-6">
+      <Categories />
+
+      
     </main>
-  );
-}
+  )};
