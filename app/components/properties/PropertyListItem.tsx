9b7ba5762c57d512f252238f7a51a31ada5fc0f3
Angelow444 <angelow.larot@msunaawan.edu.ph>
2025-11-20 22:11:18 +0800

    5th Video_Detail page for properties -Angelow B. Larot

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { PropertyType } from "./PropertyList";
+import { useRouter } from "next/navigation";
 
 interface PropertyProps {
     property: PropertyType
@@ -8,8 +9,15 @@ interface PropertyProps {
 const PropertyListItem: React.FC<PropertyProps> = ({
     property
 }) => {
+    const router = useRouter();
+
+
+
     return (
-        <div className="cursor-pointer">
+        <div 
+            className="cursor-pointer"
+            onClick={() => router.push(`/properties/${property.id}`)}
+        >
             <div className="relative overflow-hidden aspect-square rounded-xl">
                 <Image
                     fill
@@ -25,7 +33,7 @@ const PropertyListItem: React.FC<PropertyProps> = ({
             </div>
 
             <div className="mt-2">
-            <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong> per night</p>
+                <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong> per night</p>
             </div>
         </div>
     )
